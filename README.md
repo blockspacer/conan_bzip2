069484a4ad6e308b87220894c41861dd1e52be98
trofimov_d_a <derofim@y.ru>
2020-07-29 12:15:51 +0300

    UPD: sanitizers support

--- README.md
+++ README.md
@@ -2,9 +2,13 @@
 
 Modified `bzip2` recipe
 
-## EXTRA FEATURES
+* supports "openssl/OpenSSL_1_1_1-stable@conan/stable"
+* supports sanitizers, see [https://github.com/google/sanitizers/wiki/MemorySanitizerLibcxxHowTo#instrumented-gtest](https://github.com/google/sanitizers/wiki/MemorySanitizerLibcxxHowTo#instrumented-gtest)
+* uses `llvm_tools` conan package in builds with `LLVM_USE_SANITIZER`, see https://github.com/google/sanitizers/wiki/MemorySanitizerLibcxxHowTo#instrumented-gtest
+* uses `llvm_tools` conan package in builds with libc++ (will be instrumented if `LLVM_USE_SANITIZER` enabled)
+* etc.
 
-- supports "openssl/OpenSSL_1_1_1-stable@conan/stable"
+NOTE: use `-s llvm_tools:build_type=Release` during `conan install`
 
 # About
 
@@ -28,15 +32,80 @@ sudo -E docker build \
 ## Local build
 
 ```bash
+conan remote add conan-center https://api.bintray.com/conan/conan/conan-center False
+
 export PKG_NAME=bzip2/1.0.8@dev/stable
-conan remove --force $PKG_NAME
-conan create . dev/stable -s build_type=Debug --profile clang --build missing
-CONAN_REVISIONS_ENABLED=1 CONAN_VERBOSE_TRACEBACK=1 CONAN_PRINT_RUN_COMMANDS=1 CONAN_LOGGING_LEVEL=10 conan upload $PKG_NAME --all -r=conan-local -c --retry 3 --retry-wait 10 --force
+
+(CONAN_REVISIONS_ENABLED=1 \
+    conan remove --force $PKG_NAME || true)
+
+CONAN_REVISIONS_ENABLED=1 \
+    CONAN_VERBOSE_TRACEBACK=1 \
+    CONAN_PRINT_RUN_COMMANDS=1 \
+    CONAN_LOGGING_LEVEL=10 \
+    GIT_SSL_NO_VERIFY=true \
+    conan create . \
+      dev/stable \
+      -s build_type=Release \
+      --profile clang \
+      --build missing \
+      --build cascade
+
+CONAN_REVISIONS_ENABLED=1 \
+    CONAN_VERBOSE_TRACEBACK=1 \
+    CONAN_PRINT_RUN_COMMANDS=1 \
+    CONAN_LOGGING_LEVEL=10 \
+    conan upload $PKG_NAME \
+      --all -r=conan-local \
+      -c --retry 3 \
+      --retry-wait 10 \
+      --force
+```
+
+
+## Build with sanitizers support
+
+Use `-o llvm_tools:enable_tsan=True` and `-e *:compile_with_llvm_tools=True` like so:
+
+```bash
+export CC=$(find ~/.conan/data/llvm_tools/master/conan/stable/package/ -path "*bin/clang" | head -n 1)
+
+export CXX=$(find ~/.conan/data/llvm_tools/master/conan/stable/package/ -path "*bin/clang++" | head -n 1)
+
+export TSAN_OPTIONS="handle_segv=0:disable_coredump=0:abort_on_error=1:report_thread_leaks=0"
+
+# make sure that env. var. TSAN_SYMBOLIZER_PATH points to llvm-symbolizer
+# conan package llvm_tools provides llvm-symbolizer
+# and prints its path during cmake configure step
+# echo $TSAN_SYMBOLIZER_PATH
+export TSAN_SYMBOLIZER_PATH=$(find ~/.conan/data/llvm_tools/master/conan/stable/package/ -path "*bin/llvm-symbolizer" | head -n 1)
+
+# NOTE: NO `--profile` argument cause we use `CXX` env. var
+CONAN_REVISIONS_ENABLED=1 \
+    CONAN_VERBOSE_TRACEBACK=1 \
+    CONAN_PRINT_RUN_COMMANDS=1 \
+    CONAN_LOGGING_LEVEL=10 \
+    GIT_SSL_NO_VERIFY=true \
+    conan create . \
+      dev/stable \
+      -s build_type=Debug \
+      -s compiler=clang \
+      -s compiler.version=10 \
+      -s compiler.libcxx=libc++ \
+      -s llvm_tools:compiler=clang \
+      -s llvm_tools:compiler.version=6.0 \
+      -s llvm_tools:compiler.libcxx=libstdc++11 \
+      -s llvm_tools:build_type=Release \
+      -o llvm_tools:enable_tsan=True \
+      -o llvm_tools:include_what_you_use=False \
+      -e bzip2:compile_with_llvm_tools=True \
+      -e bzip2:enable_llvm_tools=True \
+      -o bzip2:enable_tsan=True
 ```
 
 ## How to diagnose errors in conanfile (CONAN_PRINT_RUN_COMMANDS)
 
 ```bash
 # NOTE: about `--keep-source` see https://bincrafters.github.io/2018/02/27/Updated-Conan-Package-Flow-1.1/
-CONAN_REVISIONS_ENABLED=1 CONAN_VERBOSE_TRACEBACK=1 CONAN_PRINT_RUN_COMMANDS=1 CONAN_LOGGING_LEVEL=10 conan create . conan/stable -s build_type=Debug --profile clang --build missing --keep-source
+CONAN_REVISIONS_ENABLED=1 CONAN_VERBOSE_TRACEBACK=1 CONAN_PRINT_RUN_COMMANDS=1 CONAN_LOGGING_LEVEL=10 conan create . conan/stable -s build_type=Debug --profile clang --build missing --build cascade --keep-source
 ```
--- conanfile.py
+++ conanfile.py
@@ -1,8 +1,21 @@
+import glob
 import os
 from conans import ConanFile, CMake, tools
+from conans.model.version import Version
+from conans.errors import ConanInvalidConfiguration
 
+from conans import ConanFile, CMake, tools, AutoToolsBuildEnvironment, RunEnvironment, python_requires
+from conans.errors import ConanInvalidConfiguration, ConanException
+from conans.tools import os_info
+import os, re, stat, fnmatch, platform, glob, traceback, shutil
+from functools import total_ordering
 
-class Bzip2Conan(ConanFile):
+# if you using python less than 3 use from distutils import strtobool
+from distutils.util import strtobool
+
+conan_build_helper = python_requires("conan_build_helper/[~=0.0]@conan/stable")
+
+class Bzip2Conan(conan_build_helper.CMakePackage):
     name = "bzip2"
     version = "1.0.8"
     url = "https://github.com/conan-io/conan-center-index"
@@ -11,10 +24,37 @@ class Bzip2Conan(ConanFile):
     description = "bzip2 is a free and open-source file compression program that uses the Burrows Wheeler algorithm."
     topics = ("conan", "bzip2", "data-compressor", "file-compression")
     settings = "os", "compiler", "arch", "build_type"
-    options = {"shared": [True, False], "fPIC": [True, False], "build_executable": [True, False]}
-    default_options = {"shared": False, "fPIC": True, "build_executable": True}
+
+    options = {
+      "enable_ubsan": [True, False],
+      "enable_asan": [True, False],
+      "enable_msan": [True, False],
+      "enable_tsan": [True, False],
+      "shared": [True, False],
+      "fPIC": [True, False],
+      "build_executable": [True, False]
+    }
+
+    default_options = {
+      "enable_ubsan": False,
+      "enable_asan": False,
+      "enable_msan": False,
+      "enable_tsan": False,
+      "shared": False,
+      "fPIC": True,
+      "build_executable": True
+    }
+
     exports_sources = "CMakeLists.txt"
     generators = "cmake"
+
+    # sets cmake variables required to use clang 10 from conan
+    def _is_compile_with_llvm_tools_enabled(self):
+      return self._environ_option("COMPILE_WITH_LLVM_TOOLS", default = 'false')
+
+    # installs clang 10 from conan
+    def _is_llvm_tools_enabled(self):
+      return self._environ_option("ENABLE_LLVM_TOOLS", default = 'false')
 
     @property
     def _source_subfolder(self):
@@ -25,8 +65,54 @@ class Bzip2Conan(ConanFile):
             del self.options.fPIC
 
     def configure(self):
-        del self.settings.compiler.libcxx
-        del self.settings.compiler.cppstd
+        lower_build_type = str(self.settings.build_type).lower()
+
+        if lower_build_type != "release" and not self._is_llvm_tools_enabled():
+            self.output.warn('enable llvm_tools for Debug builds')
+
+        if self._is_compile_with_llvm_tools_enabled() and not self._is_llvm_tools_enabled():
+            raise ConanInvalidConfiguration("llvm_tools must be enabled")
+
+        if self.options.enable_ubsan \
+           or self.options.enable_asan \
+           or self.options.enable_msan \
+           or self.options.enable_tsan:
+            if not self._is_llvm_tools_enabled():
+                raise ConanInvalidConfiguration("sanitizers require llvm_tools")
+
+        if self.options.enable_ubsan:
+            if self.options.enable_tests:
+              self.options["conan_gtest"].enable_ubsan = True
+
+        if self.options.enable_asan:
+            if self.options.enable_tests:
+              self.options["conan_gtest"].enable_asan = True
+
+        if self.options.enable_msan:
+            if self.options.enable_tests:
+              self.options["conan_gtest"].enable_msan = True
+
+        if self.options.enable_tsan:
+            if self.options.enable_tests:
+              self.options["conan_gtest"].enable_tsan = True
+
+        #del self.settings.compiler.libcxx
+        #del self.settings.compiler.cppstd
+
+    def build_requirements(self):
+        self.build_requires("cmake_platform_detection/master@conan/stable")
+        self.build_requires("cmake_build_options/master@conan/stable")
+        self.build_requires("cmake_helper_utils/master@conan/stable")
+
+        if self.options.enable_tsan \
+            or self.options.enable_msan \
+            or self.options.enable_asan \
+            or self.options.enable_ubsan:
+          self.build_requires("cmake_sanitizers/master@conan/stable")
+
+        # provides clang-tidy, clang-format, IWYU, scan-build, etc.
+        if self._is_llvm_tools_enabled():
+          self.build_requires("llvm_tools/master@conan/stable")
 
     def source(self):
         #tools.get(**self.conan_data["sources"][self.version])
@@ -40,6 +126,25 @@ class Bzip2Conan(ConanFile):
         cmake.definitions["BZ2_VERSION_STRING"] = self.version
         cmake.definitions["BZ2_VERSION_MAJOR"] = major
         cmake.definitions["BZ2_BUILD_EXE"] = "ON" if self.options.build_executable else "OFF"
+
+        cmake.definitions["ENABLE_UBSAN"] = 'ON'
+        if not self.options.enable_ubsan:
+            cmake.definitions["ENABLE_UBSAN"] = 'OFF'
+
+        cmake.definitions["ENABLE_ASAN"] = 'ON'
+        if not self.options.enable_asan:
+            cmake.definitions["ENABLE_ASAN"] = 'OFF'
+
+        cmake.definitions["ENABLE_MSAN"] = 'ON'
+        if not self.options.enable_msan:
+            cmake.definitions["ENABLE_MSAN"] = 'OFF'
+
+        cmake.definitions["ENABLE_TSAN"] = 'ON'
+        if not self.options.enable_tsan:
+            cmake.definitions["ENABLE_TSAN"] = 'OFF'
+
+        self.add_cmake_option(cmake, "COMPILE_WITH_LLVM_TOOLS", self._is_compile_with_llvm_tools_enabled())
+
         cmake.configure()
         return cmake
 
